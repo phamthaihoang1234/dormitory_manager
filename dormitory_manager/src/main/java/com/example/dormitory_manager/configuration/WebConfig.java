86b75b87a9fb92c9b6b71fe68070d0d50f6ba589
phamthaihoang1234 <namtuocbongdem1234567890@gmail.com>
2023-05-02 11:59:42 +0700

    fix bug css

---
+++ dormitory_manager/src/main/java/com/example/dormitory_manager/configuration/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.dormitory_manager.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer
+                .favorPathExtension(false)
+                .favorParameter(false)
+                .ignoreAcceptHeader(true)
+                .defaultContentType(MediaType.TEXT_HTML);
+    }
+}
