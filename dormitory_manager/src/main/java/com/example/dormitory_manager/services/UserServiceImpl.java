5783a4202d53789c869b8c297ab308692d1fdb46
phamthaihoang1234 <namtuocbongdem1234567890@gmail.com>
2023-05-05 10:26:15 +0700

    add  blog to homepage

--- dormitory_manager/src/main/java/com/example/dormitory_manager/services/UserServiceImpl.java
+++ dormitory_manager/src/main/java/com/example/dormitory_manager/services/UserServiceImpl.java
@@ -1,68 +1,100 @@
 package com.example.dormitory_manager.services;
 
+import com.example.dormitory_manager.Repository.UseRepository;
 import com.example.dormitory_manager.entities.UserInfo;
-import com.example.dormitory_manager.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.dormitory_manager.entities.UserPrinciple;
+import org.springframework.context.MessageSource;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
+    private final UseRepository userRepository;
 
-    @Autowired
-    private UserRepository userRepository;
+    private final MessageSource messageSource;
+
+    private final com.example.dormitory_manager.Services.RoleService roleService;
+
+    public UserServiceImpl(UseRepository userRepository, MessageSource messageSource, com.example.dormitory_manager.Services.RoleService roleService) {
+        this.userRepository = userRepository;
+        this.messageSource = messageSource;
+        this.roleService = roleService;
+    }
 
     @Override
-    public List<UserInfo> getAllUsers() {
+    public Iterable<UserInfo> findAll() {
         return userRepository.findAll();
     }
 
     @Override
-    public UserInfo getUserById(Long id) {
-        Optional<UserInfo> user = userRepository.findById(id);
-        return user.orElse(null);
-    }
-    
-        
-    @Override
-    public UserInfo createUser(UserInfo user) {
-        UserInfo newUser = new UserInfo();
-        newUser.setUsername(user.getUsername());
-        newUser.setPassword(user.getPassword());
-        newUser.setRoles(user.getRoles());
-        return userRepository.save(newUser);
+    public Optional<UserInfo> findById(Long id) {
+        return userRepository.findById(id);
     }
 
+
     @Override
-    public UserInfo updateUser(Long id, UserInfo user) {
-        Optional<UserInfo> optionalUser = userRepository.findById(id);
-        if (optionalUser.isPresent()) {
-            UserInfo existingUser = optionalUser.get();
-            existingUser.setUsername(user.getUsername());
-            existingUser.setPassword(user.getPassword());
-            existingUser.setRoles(user.getRoles());
-            return userRepository.save(existingUser);
-        } else {
-            return null;
-        }
+    public UserInfo findByEmail(String email) {
+        return userRepository.findByEmail(email);
     }
 
     @Override
     public UserInfo findByUserName(String username) {
         return userRepository.findByUsername(username);
     }
-    
+
     @Override
-    public boolean deleteUser(Long id) {
-        Optional<UserInfo> optionalUser = userRepository.findById(id);
-        if (optionalUser.isPresent()) {
-            userRepository.deleteById(id);
-            return true;
-        } else {
-            return false;
-        }
+    public boolean existsByUsername(String username) {
+        return userRepository.existsByUsername(username);
     }
 
+    @Override
+    public Optional<UserInfo> existsByUsernameAndPassword(String email, String password) throws Exception {
+        return userRepository.existsByUsernameAndPassword(email,password);
+    }
+
+    @Override
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public UserInfo save(UserInfo user) throws Exception {
+
+        return userRepository.save(user);
+    }
+
+    @Override
+    public boolean changePassword(UserInfo user) throws Exception{
+        return true;
+
+    }
+
+    @Override
+    public UserInfo updateInfor(UserInfo user) throws Exception{
+        user.setName(user.getName());
+        user.setEmail(user.getEmail());
+        user.setPhoneNumber(user.getPhoneNumber());
+        user.setGender(user.getGender());
+
+        user.setAddress(user.getAddress());
+        return userRepository.save(user);
+    }
+
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        System.out.println("vao ham load");
+        UserInfo user = userRepository.findByUsername(username);
+        System.out.println(user.getPassword());
+        if(user == null){
+            throw new UsernameNotFoundException(username);
+        }
+        return UserPrinciple.built(user);
+
+
+    }
 }
